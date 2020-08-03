a22e8a94f7c1f94abc33ec6d047c1ad827d15663
Pavel Kavaliuk <voorhis365@mail.ru>
2020-08-03 22:18:31 +0300

    Initial commit

---
+++ src/main/java/com/example/pavel/kavaliuk/dockerunderstanding/DockerUnderstandingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pavel.kavaliuk.dockerunderstanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerUnderstandingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerUnderstandingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pavel/kavaliuk/dockerunderstanding/DockerUnderstandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pavel.kavaliuk.dockerunderstanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerUnderstandingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
